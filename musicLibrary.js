6e759726241ed051b613fd9ac89d8aad551c2d9b
vagrant <you@example.com>
2019-06-07 22:40:31 +0000

    finished one playlist with two tracks

--- musicLibrary.js
+++ musicLibrary.js
@@ -1,1 +1,74 @@
-musicLibrary2.js
+function library(name, creator) {
+    this.name = name,
+        this.creator = creator,
+        this.playlists = [];
+};
+
+library.prototype.addPlaylist = function addPlaylist(playlist) {
+    this.playlists.push(playlist);
+};
+
+function playlist(name) {
+    this.name = name;
+    this.tracks = [];
+    this.playlistRating = 0;
+    this.totalDuration = 0;
+}
+
+
+playlist.prototype.updatePlaylistRating = function() {
+    this.playlistRating = 0;
+    for (let track = 0; track < this.tracks.length; track++) {
+        this.playlistRating += this.tracks[track].rating;
+    }
+    this.playlistRating = this.playlistRating / this.tracks.length;
+};
+
+
+playlist.prototype.updateTotalDuration = function() {
+    this.totalDuration = 0;
+    for (let track = 0; track < this.tracks.length; track++) {
+        this.totalDuration += this.tracks[track].length;
+    }
+    this.totalDuration = this.totalDuration / this.tracks.length;
+};
+
+function track(title, rating, length) {
+    this.title = title,
+        this.rating = rating,
+        this.length = length
+}
+
+playlist.prototype.addTrack = function addTrack(track) {
+    this.tracks.push(track);
+    this.updatePlaylistRating();
+    this.updateTotalDuration();
+};
+
+
+let firstLibrary = new library('new', 'Devan');
+let firstPlaylist = new playlist("awesomeness");
+let firstTrack = new track('Cecilia', 5, 120);
+let secondTrack = new track('Michelle', 4, 180);
+
+
+
+console.log(firstLibrary);
+console.log(firstPlaylist);
+console.log(firstTrack);
+
+firstLibrary.addPlaylist(firstPlaylist);
+
+firstPlaylist.addTrack(firstTrack);
+firstPlaylist.addTrack(secondTrack);
+
+
+console.log(firstPlaylist);
+console.log(firstPlaylist.playlistRating);
+console.log(firstPlaylist.totalDuration);
+console.log(firstLibrary)
+
+
+// A Track has a title, a rating (an integer from 1 to 5) and a length (integer in seconds) all of which are required when creating a new track
+// Each Playlist also has an overallRating function which will calculate the rating by averaging the rating of its tracks
+// Each Playlist also has a totalDuration function which will sum the duration of all of its track
